36c157d7fd24c6cc67df27ba6130791de290e41f
sesteves <sroesteves@gmail.com>
2015-06-03 04:15:23 +0100

    Fixed JSON format.

--- src/pt/inescid/gsd/art/Main.scala
+++ src/pt/inescid/gsd/art/Main.scala
@@ -1,7 +1,6 @@
 package pt.inescid.gsd.art
 
 import argonaut._, Argonaut._
-import scalaz.\/
 
 /**
  * Created by sesteves on 02-06-2015.
@@ -12,24 +11,17 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     val jsonStr = scala.io.Source.fromFile("sla").getLines.mkString
-
-    // val result: String \/ Json = Parse.parse(jsonStr)
-
-
     val slas = jsonStr.decodeOption[List[SLA]].getOrElse(Nil)
 
     println(slas)
 
-
   }
 
-  case class SLA(application: String, maxExecTime: Double, accuracy: Double, minAccuracy: Double,
-                 cost: Double, maxCost: Double)
+}
+case class SLA(application: String, maxExecTime: Double, accuracy: Option[Double], minAccuracy: Option[Double],
+               cost: Option[Double], maxCost: Option[Double])
 
-  object SLA {
-    implicit def SLACodecJson: CodecJson[SLA] =
-      casecodec6(SLA.apply, SLA.unapply)("application", "maxExecTime", "accuracy", "minAccuracy", "cost", "maxCost")
-  }
-
-
+object SLA {
+  implicit def SLACodecJson: CodecJson[SLA] =
+    casecodec6(SLA.apply, SLA.unapply)("application", "maxExecTime", "accuracy", "minAccuracy", "cost", "maxCost")
 }
