a177d9dfe5a261d2e258cdeb53dd959f1c5b883c
sesteves <sroesteves@gmail.com>
2015-06-04 19:32:14 +0100

    Improvements

--- mvn-install.sh
+++ mvn-install.sh
@@ -1,1 +1,2 @@
+sbt package
 mvn install:install-file -DgroupId=pt.inescid.gsd -DartifactId=art -Dversion=1.0 -Dpackaging=jar -Dfile=target/scala-2.10/art_2.10-0.1-SNAPSHOT.jar
--- src/main/scala/pt/inescid/gsd/art/ArtManager.scala
+++ src/main/scala/pt/inescid/gsd/art/ArtManager.scala
@@ -7,18 +7,35 @@ import org.apache.spark.streaming.StreamingContext
  */
 class ArtManager(ssc: StreamingContext) extends Runnable {
 
+  var windowSize = 10000
+  var delay: Long = -1
+  var execTime: Long = -1
+
 
   def run() {
     while(true) {
 
-      // observe batch execution time
+      // if workload is not stable
+      if(execTime > windowSize) {
+        // add resources
+        // ssc.checkpoint()
+        ssc.stop()
 
 
 
+
+        ssc.start()
+
+      }
 
       Thread.sleep(1000)
     }
   }
 
+
+  def updateExecutionTime(delay: Long, execTime: Long) {
+    this.delay = delay
+    this.execTime = execTime
+  }
 
 }
