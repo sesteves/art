24bf272c33bf757d9f258efc72fb2c5e9bad4e2a
sesteves <sroesteves@gmail.com>
2015-06-17 20:51:39 +0100

    Added debugging messages.

--- src/main/scala/pt/inescid/gsd/art/ArtManager.scala
+++ src/main/scala/pt/inescid/gsd/art/ArtManager.scala
@@ -1,11 +1,16 @@
 package pt.inescid.gsd.art
 
 import org.apache.spark.streaming.StreamingContext
+import org.slf4j.Logger
 
 /**
  * Created by sesteves on 03-06-2015.
  */
 class ArtManager(ssc: StreamingContext) extends Runnable {
+
+  println("ART MANAGER ACTIVATED!")
+
+  private var log : Logger = null
 
   var windowSize = 10000
   var delay: Long = -1
@@ -15,14 +20,18 @@ class ArtManager(ssc: StreamingContext) extends Runnable {
   def run() {
     while(true) {
 
+      println(s"ART Delay: $delay, ExecTime: $execTime")
+
       // if workload is not stable
       if(execTime > windowSize) {
+
+        println("ART ExecTime > WindowSize")
+
         // add resources
         // ssc.checkpoint()
         ssc.stop()
 
-
-
+        ssc.sparkContext.requestExecutors(1)
 
         ssc.start()
 
@@ -34,6 +43,9 @@ class ArtManager(ssc: StreamingContext) extends Runnable {
 
 
   def updateExecutionTime(delay: Long, execTime: Long) {
+
+    println(s"ART updateExecutionTime called!")
+
     this.delay = delay
     this.execTime = execTime
   }
